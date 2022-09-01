54f1386f93eb5330f9db986721fd09b401c7d1f3
Trace Dugar <tracedugar89@gmail.com>
2022-08-31 19:03:31 -0700

    update

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ let usersName = prompt('Who are you?');
 let message;
 
 if (usersName == 'Kassie'){
-  message = '  Thank you for being there to help me at the beginning of my coding journey, '
+  message = ', Thank you for being there to help me at the beginning of my coding journey, '
 } else if (usersName == '') {
   message = "Hi No Name! ";
 } else {
-  message = ' Welcome to my site!';
+  message = ', Welcome to my site!';
 }
 
 document.write('Hi ' + usersName);
