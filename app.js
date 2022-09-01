889feaa617624d07b23860dcdf3a80fbacb96eee
Trace Dugar <tracedugar89@gmail.com>
2022-08-31 19:07:15 -0700

    last one for the day

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ let message;
 if (usersName == 'Kassie'){
   message = ', Thank you for being there to help me at the beginning of my coding journey, '
 } else if (usersName == '') {
-  message = "Hi No Name! ";
+  message = ", No Name! ";
 } else {
   message = ', Welcome to my site!';
 }
