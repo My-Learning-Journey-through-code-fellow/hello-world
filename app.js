5f7e35e7f740301c6636ab49aefc923ad5609cfa
Trace Dugar <tracedugar89@gmail.com>
2022-09-01 16:43:44 -0700

    lab 06, 07, & 08 update

--- app.js
+++ app.js
@@ -2,23 +2,30 @@
 
 let myName;
 
-myName = 'Trace';
-console.log(myName);
-
-myName = 'Chris';
-console.log(myName);
-
-let usersName = prompt('Who are you?');
+let usersName;
 
 let message;
 
-if (usersName == 'Kassie'){
-  message = ', Thank you for being there to help me at the beginning of my coding journey, '
-} else if (usersName == '') {
-  message = ", No Name! ";
-} else {
-  message = ', Welcome to my site!';
+function getName(){
+usersName = prompt('Who are you?');
+document.write ('Hi, ' + usersName);
 }
 
-document.write('Hi ' + usersName);
-document.write(message)
+function makeMessage(){
+    if (usersName == 'Kassie'){
+    message = ', Thank you for being there to help me at the beginning of my coding journey, '
+    } else if (usersName == '') {
+    message = ", No Name! ";
+    } else {
+    message = ', Welcome to my site!';
+    }
+    document.write(message)
+}
+
+function showDisc(){
+    let usersNum = prompt('How would you rate my page? 1-5');
+    
+    for (let i = 0; i < usersNum && i < 5; i++){
+      document.write('<img src="disk.jpg" alt="Compact Disc" class="Disc"/>');
+    }
+  }
